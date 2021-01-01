312fac2235c15042465592163765eea44dcbf7b0
Bert Witzel <bwitzel82@gmail.com>
2021-01-01 15:06:45 -0500

    update js, styles, and html

--- js/app.js
+++ js/app.js
@@ -25,5 +25,63 @@ window.addEventListener('click', (e) => {
   if (e.target == modal) {
     modal.style.display = 'none';
   }
-})
+});
+
+// create note function
+const modalColors = document.querySelector('.modal-colors');
+const colorClass = [
+  'mod-1', 'mod-2', 'mod-3', 'mod-4', 'mod-5', 'mod-6',
+  'mod-7', 'mod-8', 'mod-9', 'mod-10', 'mod-11', 'mod-12', 'mod-13', 
+  'mod-14', 'mod-15', 'mod-16', 'mod-17', 'mod-18'
+];
+modalColors.addEventListener('click', (e) => {
+  const col = document.getElementsByClassName('col');
+  diary.insertBefore(div, initialDiv);
+  div.appendChild(section);
+  section.appendChild(span);
+  for (let i = 0; i < col.length; i++) {
+    if (e.target === col[i]) {
+      span.className = colorClass[i];
+    }
+  }
+});
+const form = document.querySelector('.form-2');
+const title = document.querySelector('#title-input');
+const text = document.querySelector('#text-input');
+const date = new Date();
+const monthNames = [
+  "January", "February", "March", "April", "May", "June", "July",
+  "August", "September", "October", "November", "December"
+];
+
+const month = monthNames[date.getMonth()];
+const day = date.getDate();
+const year = date.getFullYear();
+const noteDate = `
+  ${month} ${day}, ${year}
+`;
+const diary = document.querySelector('.diary');
+const initialDiv = diary.firstElementChild;
+const div = document.createElement('div');
+const section = document.createElement('section');
+const span = document.createElement('span');
+const article = document.createElement('article');
+const h2 = document.createElement('h2');
+const h3 = document.createElement('h3');
+const p = document.createElement('p');
+
+postButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  section.appendChild(h3);
+  div. appendChild(article);
+  article.appendChild(h2);
+  article.appendChild(p);
+  h2.textContent = title.value;
+  h3.textContent = noteDate;
+  p.textContent = text.value;
+  form.reset();
+  modal.style.display = 'none';
+});
+
 
