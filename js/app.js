71702fbe916d7c966d45a4439c25dc9eabfd6e5d
Bert Witzel <bwitzel82@gmail.com>
2021-01-02 12:35:48 -0500

    update js, html, css. Radio buttons and DOM insertion

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ const overlayNav = document.getElementById('overlayNav');
 const closeBtn = document.querySelector('.closebtn');
 const modalButton = document.getElementById('modalButton');
 const modal = document.querySelector('.modal');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+// make radio buttons invisible
+for ( let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].style.opacity = '0';
+}
 
 // open and close the navigation on mobile devices
 menu.addEventListener('click', () => {
@@ -20,24 +26,25 @@ modalButton.addEventListener('click', () => {
   modal.classList.toggle('show');
 })
 
-// create note color
+// note label colors
 const modalColors = document.querySelector('.modal-colors');
-const col = document.getElementsByClassName('col');
 const colorClass = [
   'mod-1', 'mod-2', 'mod-3', 'mod-4', 'mod-5', 'mod-6',
   'mod-7', 'mod-8', 'mod-9', 'mod-10', 'mod-11', 'mod-12', 'mod-13', 
   'mod-14', 'mod-15', 'mod-16', 'mod-17', 'mod-18'
 ];
 
-// choose a color and create part of the note
-// modalColors.addEventListener('click', (e) => {
-//   for (let i = 0; i < col.length; i++) {
-//     if (e.target === col[i]) {
-//       col[i].style.boxShadow = "0 0 5px 3px rgba(0,0,0,0.3)";
-//       span.className = colorClass[i];
-//     }
-//   }
-// });
+// choose a label color 
+modalColors.addEventListener('click', () => {
+  const col = document.getElementsByClassName('col');
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+        radioButtons[i].parentNode.style.boxShadow = "0 0 5px 5px rgba(0,0,0,0.3)";
+      } else if (!radioButtons[i].checked) {
+        radioButtons[i].parentNode.style.boxShadow = "none";
+      }
+    } 
+});
 
 // use JS to get the current date
 const date = new Date();
@@ -52,27 +59,24 @@ const year = date.getFullYear();
 const noteDate = `
   ${month} ${day}, ${year}
 `;
+
 // create note
 const form = document.querySelector('.form-2');
 const title = document.querySelector('#title-input');
 const text = document.querySelector('#text-input');
 const newNote = document.getElementById('newNote');
-const diary = document.querySelector('.diary');
-const initialDiv = diary.firstElementChild;
-const div = document.createElement('div');
-const section = document.createElement('section');
-const span = document.createElement('span');
-const article = document.createElement('article');
-const h2 = document.createElement('h2');
-const h3 = document.createElement('h3');
-const p = document.createElement('p');
 
-// post the note for final product
-const postButton = document.getElementById('postButton');
-const cancelButton = document.getElementById('cancelButton');
-
+// function to post a note
 function createDiv() {
-  newNote.appendChild(div);
+  const initialDiv = newNote.firstElementChild;
+  const div = document.createElement('div');
+  const section = document.createElement('section');
+  const span = document.createElement('span');
+  const article = document.createElement('article');
+  const h2 = document.createElement('h2');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+  newNote.insertBefore(div, initialDiv);
   div.appendChild(section);
   section.appendChild(span);
   section.appendChild(h3);
@@ -82,19 +86,41 @@ function createDiv() {
   h2.textContent = title.value;
   h3.textContent = noteDate;
   p.textContent = text.value;
+  let radioValue = '';
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+        radioValue = radioButtons[i].value;
+      } 
+  } 
+  span.className = radioValue;
 }
 
+// post a note or cancel
+const postButton = document.getElementById('postButton');
+const cancelButton = document.getElementById('cancelButton');
+
+// function to uncheck buttons
+function uncheckButton() {
+  for (let i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].parentNode.style.boxShadow = 'none';
+  } 
+}
+
+// post note
 postButton.addEventListener('click', (e) => {
   e.preventDefault();
   createDiv();
+  uncheckButton();
   form.reset();
   modal.classList.toggle('show');
 })
 
+// cancel button 
 cancelButton.addEventListener('click', (e) => {
   e.preventDefault();
-  modal.classList.toggle('show');
   form.reset();
+  uncheckButton();
+  modal.classList.toggle('show');
 })
 
 
